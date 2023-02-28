0f8b0b31f3ea00666a77d5da75361dd604ab1a07
emilbekovna <n.abdyraeva25@gmail.com>
2023-02-28 15:56:20 +0600

    Initial commit

---
+++ src/Java.java
@@ -0,0 +1,8 @@
+public class Java {
+
+    public void me(){
+        int z = 5/0;
+        System.out.println(z);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+
+       throw new Exception();
+
+
+
+    }
+}
+
+
+
